189bdeb1fddaf3a35342ea98668a3a30b6f5a7cf
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-05-03 10:28:13 +0530

    sss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { BindComponent } from './bind/bind.component';
-import { CouterComponent } from './couter/couter.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BindComponent,
-    CouterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+ 
+ }
