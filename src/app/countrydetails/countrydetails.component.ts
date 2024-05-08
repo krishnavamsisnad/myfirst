e9d6f79260a0099a17bb89b204aaf8eb1960c7c5
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-05-08 12:50:16 +0530

    ss

--- src/app/countrydetails/countrydetails.component.ts
+++ src/app/countrydetails/countrydetails.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-countrydetails',
   templateUrl: './countrydetails.component.html',
   styleUrls: ['./countrydetails.component.css']
 })
 export class CountrydetailsComponent implements OnInit {
-  cname: any;
-  countrydetails: any;
 
-  constructor(public router: ActivatedRoute) {}
+  countrycode: any;
+  conutrydetailes:any;
+  constructor(public router: ActivatedRoute, public http:HttpClient) {}
 
   ngOnInit() {
-    this.router.queryParams.subscribe((qp) => {
-      this.countrydetails = qp[this.cname];
+    this.router.params.subscribe((qp) => {
+      console.log(qp)
+      this.countrycode=qp["cname"]
+      this.http.get(`https://restcountries.com/v3/alpha/${this.countrycode}`).subscribe((data)=>{
+        console.log(data)
+        this.conutrydetailes=data
+      })
     });
   }
 }
