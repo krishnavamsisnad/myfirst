23dcd48ee9ff79d87114e0f2057c519c43fc7695
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-05-09 10:41:19 +0530

    ss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SubDataComponent } from './data/sub-data/sub-data.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"product",
+    component:SubDataComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
