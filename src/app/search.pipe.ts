b47bfb535c85e0e6eea10debe240020aea50abc9
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-05-14 15:06:20 +0530

    ss

--- src/app/search.pipe.ts
+++ src/app/search.pipe.ts
@@ -5,10 +5,16 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
 
-  transform(value:any, args:any[]): any{
+  transform(value:any, args:string): any{
+  if(args){
     return value.filter((a:any)=>{
-return 
-    });
+      return JSON.stringify(a).toLowerCase().includes(args.toLowerCase())
+    })
+  }
+  else{
+    return value
+  }
+
   }
 
 }
