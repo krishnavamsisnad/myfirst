1f936ed3e4a402f55eb1d5bc2b6fb10c8c50ef1f
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-05-15 12:27:34 +0530

    ss

--- src/app/countrys/countrys.component.ts
+++ src/app/countrys/countrys.component.ts
@@ -10,10 +10,13 @@ export class CountrysComponent {
   
 constructor(public cs:CoutryapiService){}
 country:any
+error:any
 ngOnInit(){
   this.cs.getcountry().subscribe((data)=>{
     console.log(data)
     this.country=data
+  },(error)=>{
+    this.error=error
   })
 }
 }
