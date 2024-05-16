47dbbab7841f6972b817a7ef328e6f22d28b4e08
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-05-16 14:39:53 +0530

    ss

--- src/app/buynow/buynow.component.ts
+++ src/app/buynow/buynow.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl ,FormGroup,Validators,FormArray} from '@angular/forms';
 
 @Component({
   selector: 'app-buynow',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class BuynowComponent {
 
+  studentfrom=new FormGroup({
+    fristname:new FormControl('',[Validators.required]),
+    lastname:new FormControl("",[Validators.required,Validators.minLength(2)]),
+    phonenume:new FormArray([new FormControl(),new FormControl()])
+  })
+  get phonenume(){
+    return this.studentfrom.get("phonenume") as FormArray
+  }
+  add(){
+    console.log(this.studentfrom.value)
+  }
+  addother(){
+    this.phonenume.push(new FormControl())
+  }
 }
