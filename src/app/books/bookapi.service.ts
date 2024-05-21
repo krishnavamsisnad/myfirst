446061c1afd3e69c346ccbda2c8ea05a1d67f071
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-05-21 14:29:19 +0530

    ss

--- src/app/books/bookapi.service.ts
+++ src/app/books/bookapi.service.ts
@@ -4,9 +4,19 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class BookapiService {
-
+  bookapi='http://localhost:3000/posts'
   constructor(public http:HttpClient) { }
   getallbooks(){
-    return this.http.get('http://localhost:3000/posts')
+
+    return this.http.get(this.bookapi)
+  }
+  addbook(newbook:any){
+    return this.http.post(this.bookapi,newbook)
+  }
+  deletbook(id:any){
+    return this.http.delete(`${this.bookapi}/${id}`)
+  }
+  editbook(id:any,newbook:any){
+    return this.http.put(`${this.bookapi}/${id}`,newbook)
   }
 }
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -7,12 +7,55 @@ import { BookapiService } from './bookapi.service';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent {
+  book:any;
+  bookId:any;
+  booktitle:any;
+ 
   constructor(public api:BookapiService){}
 ngOnInit(){
+this.getbook()
+}
+
+getbook(){
   this.api.getallbooks().subscribe({
     next:(data)=>{
       console.log(data)
+      this.book=data
     }
+  })
+}
+
+addnew(){
+  let newbooks={
+    "id":this.bookId,
+    "title":this.booktitle
+  }
+  this.api.addbook(newbooks).subscribe((res)=>{
+    console.log(res)
+    this.getbook();
+  })
+
+}
+deleted(id:any){
+this.api.deletbook(id).subscribe((res)=>{
+  console.log(res)
+  this.getbook()
+})
+}
+editbook(book:any){
+  this.bookId=book.id;
+  this.booktitle=book.title;
+
+
+}
+update(){
+let upatebook={
+  "id":this.bookId,
+  "title":this.booktitle
+
+}
+  this.api.editbook(this.bookId,upatebook).subscribe((res)=>{
+    this.getbook()
   })
 }
 }
