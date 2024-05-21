a380e6a0a21b08b536885a101d150020fbf9228f
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-05-21 15:30:26 +0530

    s

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ const routes: Routes = [
     path:'',component:HomeComponent
   },
   {
-    path:"shared",loadChildren:()=>{import("./shared/shared.module").then(x=>x.SharedModule)}
+    path:"shared",loadChildren:()=>import("./shared/shared.module").then(x=>x.SharedModule)
   }
 ];
 
