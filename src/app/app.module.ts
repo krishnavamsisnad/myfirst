6184bed06bc2c805e0e787d6d577f2b50ac411a2
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-05-23 15:26:17 +0530

    ss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import {ToastrModule} from "ngx-toastr"
+import {ToastrModule} from "ngx-toastr";
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/employe/employe.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EmployedetailesComponent } from './employedetailes/employedetailes.component';
+
+
+
+@NgModule({
+  declarations: [
+    EmployedetailesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class EmployeModule { }
