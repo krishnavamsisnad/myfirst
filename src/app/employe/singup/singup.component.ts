39d4b3a9cfc721afcd48ba20c8fbee63ba1b72ee
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-05-24 18:00:50 +0530

    ss

--- src/app/employe/singup/singup.component.ts
+++ src/app/employe/singup/singup.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-singup',
   templateUrl: './singup.component.html',
@@ -9,9 +10,12 @@ import { HttpClient } from '@angular/common/http';
 export class SingupComponent {
   username=""
   passwoard=""
-  constructor(public http:HttpClient){}
+  constructor(public http:HttpClient,public r:Router){}
   singin(f:NgForm){
-    
+    this.http.post('http://localhost:3000/singin',f.value).subscribe((res)=>{
+      console.log(res)
+      this.r.navigateByUrl('')
+  })
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +11,12 @@ import { FormBuilder, FormGroup, NgForm } from '@angular/forms';
 export class HomeComponent {
   username=""
   passwoard=""
+  constructor(public http:HttpClient,public router:Router){}
 login(user:NgForm){ 
+  this.http.get('http://localhost:3000/singin').subscribe((res)=>{
+    console.log(res)
+    
+  })
   console.log(user.value)
 }
 }
