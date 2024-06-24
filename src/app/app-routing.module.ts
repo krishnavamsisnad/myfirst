ee06a2099e12ab6da3dba4df6b9196dec7dea79b
krishnavamsisnad <krishnavamsi.nalluri@snaddevelopers.com>
2024-06-24 18:14:20 +0530

    ss

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PostlistComponent } from './posts/postlist/postlist.component';
+import { AddpostComponent } from './posts/addpost/addpost.component';
+import { EditpostComponent } from './posts/editpost/editpost.component';
+import { SinglepostComponent } from './posts/singlepost/singlepost.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'',component:HomeComponent},
+  {path:'posts',component:PostlistComponent},
+  {path:'post/add',component:AddpostComponent},
+  {path:'edit/post/id',component:EditpostComponent},
+  {path:'post/detailes/id',component:SinglepostComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostlistComponent } from './posts/postlist/postlist.component';
+import { SinglepostComponent } from './posts/singlepost/singlepost.component';
+import { EditpostComponent } from './posts/editpost/editpost.component';
+import { AddpostComponent } from './posts/addpost/addpost.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostlistComponent,
+    SinglepostComponent,
+    EditpostComponent,
+    AddpostComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
